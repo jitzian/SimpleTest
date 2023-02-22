2525cc427c5101970054413ba64c0f1dd7683dde
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-02-22 15:15:44 -0500

    Implemented RecyclerView for jitzian/SimpleTest

--- app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
+++ app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
@@ -1,11 +1,24 @@
 package org.com.testing.with.simpletest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.com.testing.with.simpletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel.data.observe(this){
+            val adapter = RVCustomAdapter(it)
+            binding.mRecyclerView.adapter = adapter
+            binding.mRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        }
     }
 }
--- app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
+++ app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
@@ -1,10 +1,36 @@
 package org.com.testing.with.simpletest
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import org.com.testing.with.simpletest.databinding.CardViewItemBinding
+
 /**
  * TODO: Implement the Adapter that will populate a RecyclerView using the information generated in ViewModel
  * */
 
-class RVCustomAdapter {
+class RVCustomAdapter(private val dataset: List<Article>): RecyclerView.Adapter<RVCustomAdapter.ArticleViewHolder>() {
+
+    class ArticleViewHolder(private val binding: CardViewItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bindData(itemViewType: Article) {
+            binding.apply {
+                mTextViewTitle.text = itemViewType.title
+                mTextViewContent.text = itemViewType.content
+                Picasso.Builder(mImageViewCardViewItem.context).build().load(itemViewType.imageURL).into(mImageViewCardViewItem)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(CardViewItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount() = dataset.size
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        holder.bindData(dataset[position])
+    }
 
 
 }
